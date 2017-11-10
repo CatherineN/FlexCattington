2ce9064e368039585255a763e043c7f3a5bf1435
MAIN\mms1944 <massive sexy>
2017-11-10 16:17:54 -0500

    goddamn

---
+++ FlexCattington/Assets/Scripts/WaiterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaiterScript : MonoBehaviour {
+
+    public enum States
+    {
+        waiterApproach,
+        questionPopped,
+        possession,
+        aftermath,
+    }
+    private Transform waiterTrans;
+    private States gameState;
+	// Use this for initialization
+	void Start () {
+        waiterTrans = gameObject.transform;
+        gameState = States.waiterApproach;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
