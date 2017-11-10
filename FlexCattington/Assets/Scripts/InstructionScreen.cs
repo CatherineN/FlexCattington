7d635436d4d826e51f289b00c7469130ea78d19d
mcl1601 <mcl1601@IGM2350-20>
2017-11-09 20:45:51 -0500

    Added the instruction screen

---
+++ FlexCattington/Assets/Scripts/InstructionScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionScreen : MonoBehaviour
+{
+    public Sprite screen1;
+    public Sprite screen2;
+
+    public Button next;
+    public Button back;
+    public Button play;
+
+    private SpriteRenderer sRender;
+	// Use this for initialization
+	void Start ()
+    {
+        sRender = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void NextScreen()
+    {
+        sRender.sprite = screen2;
+        next.gameObject.SetActive(false);
+        back.gameObject.SetActive(true);
+        play.gameObject.SetActive(true);
+    }
+
+    public void PreviousScreen()
+    {
+        sRender.sprite = screen1;
+        next.gameObject.SetActive(true);
+        back.gameObject.SetActive(false);
+        play.gameObject.SetActive(false);
+    }
+
+    public void Play()
+    {
+
+    }
+}
