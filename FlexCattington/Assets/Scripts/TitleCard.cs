d3d26237723d79f20e046caee3f658f5c34530af
mcl1601 <mcl1601@IGM2350-24>
2017-11-30 13:53:00 -0500

    Update instructions and added in title card with script to waldo

---
+++ FlexCattington/Assets/Scripts/TitleCard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCard : MonoBehaviour
+{
+    public float waitTime; // time the card is visible
+    public float fadeTime; // time the card takes to fade out
+
+    private float timer; // keeps track of time
+    private SpriteRenderer sr; // acess to the color of the sprite
+
+	// Use this for initialization
+	void Start ()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // increment time
+        timer += Time.deltaTime;
+
+        // begin to fade after a specified period of time
+        if (timer >= waitTime)
+            StartCoroutine(FadeOut());
+	}
+    
+    IEnumerator FadeOut()
+    {
+        // take as long as specified in fadeTime
+        for(float i = fadeTime; i > 0; i -= Time.deltaTime)
+        {
+            Color c = sr.color; // get the color
+            c.a = i; // 
+            sr.color = c; // set the new color
+            yield return null;
+        }
+
+        // delete the gameobject when it fades out
+        GameObject.Destroy(gameObject);
+    }
+}
