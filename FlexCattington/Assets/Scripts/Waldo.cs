bd7901b8f9282133e35085136fb46192b47ad7d4
fuck this so much <i wanna die>
2017-12-03 22:32:04 -0500

    Flex pops up disapprovingly

--- FlexCattington/Assets/Scripts/Waldo.cs
+++ FlexCattington/Assets/Scripts/Waldo.cs
@@ -14,7 +14,7 @@ public class Waldo : MonoBehaviour
 	void Start ()
     {
         wrong = GameObject.Find("WrongAnswer");
-        wrong.SetActive(false);
+        //wrong.SetActive(false);
         score = PlayerPrefs.GetFloat("score");
         //Decrease score for every second the player spends on the screen
         StartCoroutine("DecreaseScore");
@@ -39,8 +39,7 @@ public class Waldo : MonoBehaviour
             {
                 hit.transform.tag = "Not Wrapping";
                 score -= 5;
-                wrong.SetActive(true);
-                counter++;
+                StartCoroutine(ShowFlex(wrong.transform.position));
 
             }
             else if (hit.transform.tag == "Correct")
@@ -51,18 +50,35 @@ public class Waldo : MonoBehaviour
                 
 
         }
-        if (counter >= 1)
+        /*if (counter >= 1)
             counter++;
         if(counter==20)
         {
             counter = 0;
             wrong.SetActive(false);
-        }
+        }*/
 	}
     IEnumerator DecreaseScore()
     {
         score -= 0.1f;
         yield return new WaitForSeconds(1);
         StartCoroutine("DecreaseScore");
+    }
+
+    IEnumerator ShowFlex(Vector3 start)
+    {
+        for(float time = 0; time < 0.5f; time += Time.deltaTime)
+        {
+            wrong.transform.position = Vector3.Lerp(start, new Vector3(3.63f, -2.0f, -2.6f), time / 0.5f);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.25f);
+
+        for (float time = 0; time < 0.5f; time += Time.deltaTime)
+        {
+            wrong.transform.position = Vector3.Lerp(new Vector3(3.63f, -2.0f, -2.6f), start, time / 0.5f);
+            yield return null;
+        }
     }
 }
